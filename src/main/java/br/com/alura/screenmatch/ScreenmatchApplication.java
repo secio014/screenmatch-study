dcbf01a815beaf5a885ea64ac9db32c5a7ad5c52
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2025-08-27 16:29:08 -0300

    intial configs springboot study

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoAPI();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=for+all+mankind&apikey=40d31cf5");
+        System.out.println(json);
+
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados );
+    }
+}
